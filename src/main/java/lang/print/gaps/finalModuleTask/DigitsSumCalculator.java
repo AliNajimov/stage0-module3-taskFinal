6c8b1f9c9bbb713cb5ca174e46ed4189d36684d0
Muhammad Najimov <114020956+AliNajimov@users.noreply.github.com>
2022-10-22 14:00:02 +0500

    Update DigitsSumCalculator.java

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,13 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int sum=0;
+        int oneD, twoD, threeD, fourD;
+        oneD = number/1000;
+        twoD = (number%1000)/100;
+        threeD = (number%100)/10;
+        fourD = number%10;
+        sum=oneD+twoD+threeD+fourD;
+        System.out.println(sum);
     }
 }
